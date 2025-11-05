96d31051c72acc33c6eb23cc0a862b5d4ed5f615
Charles Coulombe <charles.coulombe@calculquebec.ca>
2025-11-04 21:27:42 -0500

    Updated and refactored galaxy-upload.

--- galaxy-upload.py
+++ galaxy-upload.py
@@ -1,115 +1,86 @@
 #!/usr/bin/env python3
 
-import sys,os,json
-
-from bioblend.galaxy import GalaxyInstance
-
-def parse_parameters(arguments):
-  parameters = {}
-  for arg in arguments:
-    try:
-      key, value = arg.split('=')
-      parameters[key] = value
-    except ValueError:  # Not in the format "key=value"
-      print(f"Invalid argument format: {arg}. Skipping.")
-  return parameters
-
-def select_dest_history(gi,selected_destination_hist_id):
-
-    histories=gi.histories.get_histories()
-
-    if selected_destination_hist_id==None:
-        for i, hist in enumerate(histories, start=1):
-            print(f"{i} : {hist['id']} : \"{hist['name']}\"")
-        hist_choice = input("\nEnter the number corresponding to the destination history: ")
-        selected_history=histories[int(hist_choice)-1]
-    else:
-        for hist in histories:
-            if hist['id']== selected_destination_hist_id:
-                selected_history=hist
-
-    return selected_history
+import argparse
+import os
+from bioblend import galaxy
+from dotenv import load_dotenv
+from rich.progress import track
 
 
-def select_file(provided_file_to_upload):
+def create_argparser():
+    """Create the arguments parser."""
+    parser = argparse.ArgumentParser(
+        prog="galaxy-upload",
+        description="UseGalaxy file upload utility.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    if provided_file_to_upload!=None:
-        if os.path.isfile(provided_file_to_upload):
-            return provided_file_to_upload
-        else:
-            print("ERROR: File provided is not a file !!!")
-            exit()
+    # Positional argument
+    parser.add_argument(
+        "-e",
+        "--envfile",
+        default=".env",
+        help="Configuration environment file",
+    )
 
-    startpath = os.getcwd()  # Start at the current directory
-    while True:
-        print("\nCurrent directory:", os.path.abspath(startpath))
-        print("Contents:")
-        items = os.listdir(startpath)
-        for i, item in enumerate(items, start=1):
-            full_path = os.path.join(startpath, item)
-            if os.path.isdir(full_path):
-                print(f"  {i}. {item}/")
-            else:
-                print(f"  {i}. {item}")
+    parser.add_argument(
+        "--ask-api-key",
+        default=False,
+        action="store_true",
+        help="Prompt for Galaxy API key",
+    )
 
-        user_choice = input("\nEnter a number to select an item (or '../' to go up, or 'q' to quit): ")
-        if user_choice.lower() == 'q':
-            break
-        elif user_choice == '..':
-            startpath = os.path.dirname(startpath)  # Move up one level
-        else:
-            try:
-                selected_index = int(user_choice) - 1
-                selected_item = items[selected_index]
-                new_path = os.path.join(startpath, selected_item)
-                if os.path.isdir(new_path):
-                    startpath = new_path
-                else:
-                    selected_file=new_path
-                    break
-            except (ValueError, IndexError):
-                print("Invalid input. Please enter a valid number or '../'.")
+    parser.add_argument("--url", default=None, help="Galaxy URL endpoint")
 
-    return selected_file
+    parser.add_argument(
+        "--history-id",
+        default=None,
+        required=True,
+        help="History id to filter on",
+    )
+
+    parser.add_argument(
+        "--file",
+        nargs="+",
+        required=True,
+        help="Input files path to upload.",
+    )
+
+    parser.add_argument(
+        "--checkpoints",
+        default=os.path.join(os.getcwd(), ".checkpoints"),
+        help="Checkpoints file",
+    )
+
+    return parser
 
 if __name__ == "__main__":
 
-    params = parse_parameters(sys.argv[1:])
+    # parse cli arguments
+    args = create_argparser().parse_args()
 
-    if 'key' not in params:
-        user_key = input("\nPlease provide your access secret key to access your UseGalaxy.ca account: ")
-    else:
-        user_key=params['key']
+    # read .env and set environment if envfile exists
+    load_dotenv(args.envfile)
 
-    if 'galaxyurl' not in params:
-        galaxy_url='https://usegalaxy.ca'
-    else:
-        galaxy_url=params['galaxyurl']
+    if args.ask_api_key:
+        from getpass import getpass
+        os.environ["GALAXY_API_KEY"] = getpass("UseGalaxy API key: ")
 
-    if 'histid' not in params:
-        histid = None
-    else:
-        histid=params['histid']
+    if args.url:
+        os.environ["GALAXY_URL"] = args.url
 
-    if 'filepath' not in params:
-        filepath = None
-    else:
-        filepath=params['filepath']
+    gi = galaxy.GalaxyInstance(
+        url=os.environ["GALAXY_URL"],
+        key=os.environ["GALAXY_API_KEY"],
+    )
 
-    gi = GalaxyInstance(url=galaxy_url, key=user_key )
-
-    selected_destination_hist = select_dest_history(gi,histid)
-
-    selected_file_to_upload = select_file(filepath)
-
-    print("\n====================================================================\n")
-
-    print(f"Galaxy URL where to upload file : {galaxy_url}")
-    print(f"File selected to upload         : {selected_file_to_upload}")
-    print(f"Destination history selected    : {selected_destination_hist['name']}")
-
-    user_choice = input("\nPlease confirm the operation ('y' or 'n'): ")
-    if user_choice.lower() == 'y':
-        gi.tools.upload_file(selected_file_to_upload,selected_destination_hist['id'])
-    else:
-        exit()
+    for file in track(args.file, description="[cyan]Uploading..."):
+        if os.path.exists(file):
+            gi.tools.upload_file(
+                path=file,
+                history_id=args.history_id,
+                storage=args.checkpoints,
+                auto_decompress=True,
+            )
+        else:
+            print(f'{file} does not exists...skipping!')
